8ce943d1f0b5832611626ff01105e89113178c1b
kunweb04 <enderwolf9487@gmail.com>
2025-11-11 13:45:20 +0800

    Add files via upload

--- announcement-modal.js
+++ announcement-modal.js
@@ -1,4 +1,4 @@
-const ANNOUNCEMENT_VERSION = 'v1.0.3';
+const ANNOUNCEMENT_VERSION = 'v1.0.5';
 
 document.addEventListener('DOMContentLoaded', function() {
     const announcementModal = document.getElementById('announcementModal');
